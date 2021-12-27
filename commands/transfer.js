dcf39ddbc967777687bf283873c8e474ff51ad5e
Glitch (ribbon-dashing-globe) <none>
2021-12-27 21:32:42 +0000

    🎉💿 Checkpoint ./commands/transfer.js:40441708/411 ./commands/shop.js:40441708/4

--- commands/transfer.js
+++ commands/transfer.js
@@ -1,15 +1,15 @@
 exports.execute = async (client, message, args) => {
   let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) 
   let authordata = client.eco.fetchMoney(message.author.id) 
-  if (!member) return message.channel.send('Please mention the person or give their ID') 
+  if (!member) return message.channel.send('Por favor mencione o jogador ou me de o ID') 
   let amount = args[1]
-  if (!amount || isNaN(amount)) return message.channel.send('Please enter a valid amount to transfer') 
-  if(authordata.amount < amount) return message.channel.send('Looks like you don\'t have that much money') 
+  if (!amount || isNaN(amount)) return message.channel.send('Por favor coloque uma quantia possível') 
+  if(authordata.amount < amount) return message.channel.send('parece que você não tem muito dinheiro') 
   await client.eco.transfer(message.author.id, member.id, amount) 
-  return message.channel.send(`You have successfully transferred 💸**${amount}** to ** ${member.user.tag}**.`)
+  return message.channel.send(`Você transferiu com sucesso 💸**${amount}** para ** ${member.user.tag}**.`)
 }
 exports.help = {
   name: "transferir",
-  aliases: ['give', 'share'],
-  usage: `transferir <member> <amount>`
+  aliases: ['dar', 'compartilhar'],
+  usage: `transferir <membro> <quantidade>`
 };
