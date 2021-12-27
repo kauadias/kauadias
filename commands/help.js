e16729eb89a3f23a901ea7317f8db41ba99bd7c9
Glitch (ribbon-dashing-globe) <none>
2021-12-27 20:57:36 +0000

    📇🐺 Checkpoint ./commands/help.js:40441708/155 ./commands/inventory.js:40441708/39

--- commands/help.js
+++ commands/help.js
@@ -3,9 +3,8 @@ const { MessageEmbed } = require("discord.js");
 exports.execute = async (client, message, args) => {
     const embed = new MessageEmbed()
         .setAuthor("Commands")
-        .setTitle("Make Sure To Check Out This Channel For More Such Bots")
-        .setURL("https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ")
-        .setDescription(`Total Commands: ${client.commands.size}`)
+        .setTitle("lista de comandos")
+        .setDescription(`Todos Comandos: ${client.commands.size}`)
         .setColor("BLURPLE")
         .setTimestamp()
         .setThumbnail(client.user.displayAvatarURL)
